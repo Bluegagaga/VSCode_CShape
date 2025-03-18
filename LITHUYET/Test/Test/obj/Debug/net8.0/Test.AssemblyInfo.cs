2352f18d7585342753e83be1481697b2a345b00f
Bluegagaga <anhemhuyngoc@gmail.com>
2025-03-18 13:08:48 +0700

    Test

--- LITHUYET/Test/Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ LITHUYET/Test/Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9245e247241f1877d28fe38a13dc962b98cc8bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c6c2d868d6be24c47c50a43428126a6c61fa2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
